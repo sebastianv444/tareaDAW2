9db64f7aa185bc3efbe0cb4d7fc0e6f1791f26b0
sebastian <sebastian12verositi@gmail.com>
2024-12-19 02:26:02 +0100

    70% de proyecto final de maquetacion de diseño

--- proyecto/SQL/concesionario.sql
+++ proyecto/SQL/concesionario.sql
@@ -26,6 +26,27 @@ CREATE TABLE clientes(
     nombre varchar(255) NOT NULL,
     ciudad varchar(255) NOT NULL,
     gastado int(255) NOT NULL,
-    CONSTRAINT pk_clientes PRIMARY KEY(id),
-    CONSTRAINT fk_cliente FOREIGN KEY(vendedor_id) REFERENCES vendedores(id)
+    CONSTRAINT pk_cliente PRIMARY KEY(id),
+    CONSTRAINT fk_cliente_vendedores FOREIGN KEY(vendedor_id) REFERENCES vendedores(id)
+);
+
+CREATE TABLE vendedores(
+    id int(255) auto_increment NOT NULL,
+    grupo_id int(255),
+    nombre varchar(255) NOT NULL,
+    apellidos varchar(255) NOT NULL,
+    cargo varchar(255) NOT NULL,
+    fecha_alta date NOT NULL,
+    sueldo int(255) NOT NULL,
+    comision int(255) NOT NULL,
+    jefe varchar(255) NOT NULL,
+    CONSTRAINT pk_vendedores PRIMARY KEY(id),
+    CONSTRAINT fk_vendedore_grupos FOREIGN KEY(grupo_id) REFERENCES grupos(id)
+);
+
+CREATE TABLE grupos(
+    id int(255) auto_increment NOT NULL,
+    nombre varchar(255) NOT NULL,
+    ciudad varchar(255) NOT NULL,
+    CONSTRAINT pk_grupos PRIMARY KEY(id)
 );
