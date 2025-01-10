0f2282625f11385696646c9ac3a5289e3c71de7b
sebastian <sebastian12verositi@gmail.com>
2025-01-10 12:38:49 +0100

    codeKataWebinarsAsyncAwait

--- clase_js/oppenWebinars/js/TiradaDeBolos.js
+++
@@ -1,7 +0,0 @@
-/* 
-    1. Simulamos una tirada de 9 bolos con promesas.
-    2. Cada tirada tiene un tiempo aleatorio que tarda la bola impactar no mas de 2 segundos.
-    3. Si hemos tirado los 9 bolos sera un Strike, sino informar del numero de bolos tirados.
-    4. Si no hemos tirado bolas sacar con un error el resultado.
-    5. La salida debe ser por consola.
-*/
