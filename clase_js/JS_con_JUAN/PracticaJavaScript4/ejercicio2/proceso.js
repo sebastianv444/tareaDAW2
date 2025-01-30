0624a5c0060ae63efe8fe5f444e1f4c06ee0966e
sebastian <sebastian12verositi@gmail.com>
2025-01-31 00:32:39 +0100

    cosas

--- clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio2/proceso.js
+++ clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio2/proceso.js
@@ -1,29 +1,17 @@
-function generarLetra(){
-	let letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
-	let numero = (Math.random()*15).toFixed(0);
-	return letras[numero];
-}
-	
-function colorHEX(){
-	let coolor = "";
-	for(var i=0;i<6;i++){
-		coolor = coolor + generarLetra() ;
-	}
-	return "#" + coolor;
+function cambiarFondo() {
+  let ancho = window.innerWidth;
+  let alto = window.innerHeight;
+  let color = `rgb(${ancho % 256}, ${alto % 256}, ${(ancho + alto) % 256})`;
+  document.body.style.backgroundColor = color;
 }
 
-(function main(){
-    
-    console.log(screen.height);
-    console.log(screen.width);
+/* no se ejecuta inmediatamente , le pasamos  la referencia de la función cambiarFondo 
+al addEventListener, para que JavaScript la ejecute solo cuando ocurra el evento resize. */
+/* Si le pusiera parentesis se ejecutaria inmediatamente la funcion sin que el usuario
+haga algun cambio. */
+window.addEventListener("resize", cambiarFondo);
 
-    let alturaPorDefecto = screen.height;
-    let anchoPorDefecto = screen.width;
-    
-    if (screen.height <= alturaPorDefecto || screen.width <= anchoPorDefecto) {
-        document.querySelector('h3').hidden = false;
-        document.body.style.backgroundColor = colorHEX();
-    }else{
-
-    }
-})()
+/* esta llamada sirve solo para que te ponga un color incial al entrar a la pagina, si 
+quitaramos esta linea no pasaria nada en funcionamiento a mi aplicacion, ya que esto
+solo es para que tenga un color inicial al entrar a la pagina nada mas.*/
+cambiarFondo(); 
