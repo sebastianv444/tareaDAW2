6056ee5894f86d4c3be3d50b3dfdc443814da0ce
sebastian <sebastian12verositi@gmail.com>
2025-02-05 13:37:48 +0100

    avenceEjercicio9deJS

--- clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio8/proceso.js
+++ clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio8/proceso.js
@@ -2,26 +2,41 @@ const textarea = document.querySelector("textarea");
 const avisos = document.querySelector("#avisos");
 
 function guardar() {
-  localStorage.setItem("texto", textarea.textContent.trim());
+  if (textarea.value.trim() !== "") {
+    localStorage.setItem("texto", textarea.value.trim());
+    avisos.textContent = "Texto guardado automáticamente.";
+    avisos.style.color = "green";
+    setTimeout(() => (avisos.textContent = ""), 2000);
+  } else {
+    avisos.textContent = "No hay nada que guardar.";
+    avisos.style.color = "red";
+    setTimeout(() => (avisos.textContent = ""), 2000);
+  }
 }
 
 function restaurar() {
   if (localStorage.getItem("texto") !== null) {
     textarea.value = localStorage.getItem("texto");
+    avisos.textContent = "Texto restaurado.";
+    avisos.style.color = "green";
+    setTimeout(() => (avisos.textContent = ""), 2000);
   } else {
     avisos.textContent = "No hay nada que restaurar ahora.";
     avisos.style.color = "red";
+    setTimeout(() => (avisos.textContent = ""), 2000);
   }
 }
 
-function eliminar() {}
+function eliminar() {
+  textarea.value = "";
+  localStorage.removeItem("texto");
+  avisos.textContent = "Texto borrado exitosamente.";
+  avisos.style.color = "green";
+  setTimeout(() => (avisos.textContent = ""), 2000);
+}
 
-textarea.addEventListener("keydown", () => {
-  setTimeout(() => {
-    guardar();
-  }, 3000);
-});
+textarea.addEventListener("input", guardar);
 
-document.querySelector("#restaurar").addEventListener("click",restaurar);
+document.querySelector("#restaurar").addEventListener("click", restaurar);
 
-document.querySelector("#borrar").addEventListener("click",eliminar);
+document.querySelector("#borrar").addEventListener("click", eliminar);
