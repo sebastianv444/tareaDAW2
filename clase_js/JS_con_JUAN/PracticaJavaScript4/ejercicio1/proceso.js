377404f81e9059ae1e00f5fd66dd4838862b9038
sebastian <sebastian12verositi@gmail.com>
2025-02-06 13:34:24 +0100

    avanceCarritoDeComprasEjercicio9Practica4

--- clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio1/proceso.js
+++ clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio1/proceso.js
@@ -1,5 +1,8 @@
 function generarLetra(){
 	let letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+	/* el toFixed(0) es para quitarle los decimales, si le agregara un numero distinto
+	al 0 como un 2, me cogeria solo 2 decimales despues del "." , pude haber usado Math.trunc
+	,pero opte por esto para ir probando mas cosas en js. */
 	let numero = (Math.random()*15).toFixed(0);
 	return letras[numero];
 }
--- clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio9/proceso.js
+++ clase_js/JS_con_JUAN/PracticaJavaScript4/ejercicio9/proceso.js
@@ -1,45 +1,53 @@
 const botones = document.querySelectorAll(".agregar");
+const carritoMostrar = document.querySelector(".carrito");
 let carrito = [];
 
-function agregarAlCarrito(event){
-    const datos = comprobarProducto(event);
-    carrito.push(datos);
-    /* console.log(datos); */
+function agregarAlCarrito(event) {
+  const datos = comprobarProducto(event);
+  if (!datos) return console.log("error no existe el producto");
+  carrito.push(datos);
 
-    const ul = document.querySelector('#lista-carrito')
-    carrito.forEach(producto => {
-        console.log('id: ' + producto.id,'nombre: ' + producto.nombre)
-        let li = document.createElement("li");
-        let article1 = document.createElement("article").textContent = `${producto.nombre}`;
-        let article2 = document.createElement("article").textContent = `${producto.precio}`;
-        li.appendChild(article1);
-        li.appendChild(article2);
-        ul.appendChild(li);
-    });
+  const li = document.createElement("li");
+  li.textContent = `${datos.nombre} - €${datos.precio.toFixed(2)}`;
+
+  const btnEliminar = document.createElement("button");
+  btnEliminar.textContent = "X";
+  btnEliminar.classList.add("eliminar");
+  btnEliminar.setAttribute("data-id", datos.id);
+
+  li.appendChild(btnEliminar);
+  document.getElementById("lista-carrito").appendChild(li);
+  li.querySelector(".eliminar").addEventListener("click", eliminarDelCarrito);
+  console.log(carrito);
+
+  //ver el dinero en tiempo real
+
+  carrito.forEach((producto) => {
+    console.log(producto.precio);
+    carritoMostrar.querySelector("#total")
+  });
 }
 
-function comprobarProducto(event){
-    //selecciona al elemento padre del boton clickado.
-    const producto = event.target.parentElement;
+function comprobarProducto(event) {
+  //selecciona al elemento padre del boton clickado.
+  const producto = event.target.parentElement;
 
-    const id = producto.children[0].alt;
-    const nombre = producto.children[1].textContent;
-    const precio = producto.children[2].textContent;
-    let idEspecifico = 0;
+  const id = parseInt(producto.dataset.id);
+  const nombre = producto.querySelector("h3").textContent.trim();
+  const precioTexto = producto.querySelector(".precio").textContent.trim();
+  const precio = parseFloat(precioTexto.replace("€", ""));
 
-    for (let i = 1; i <= 3; i++) {
-     if(id.indexOf(`${i}`) !== -1){
-        idEspecifico = i;
-        console.log("se guardo con el id: ",idEspecifico);
-        return {
-            id: idEspecifico,
-            nombre: nombre,
-            precio: precio
-        }
-     }
-    }
+  return { id, nombre, precio };
 }
 
-botones.forEach(boton => {
-    boton.addEventListener("click",agregarAlCarrito);
+function eliminarDelCarrito(event) {
+  const id = event.target.dataset.id;
+  carrito.forEach((objeto) => {
+    objeto;
+  });
+  console.log(event.target.dataset.id);
+}
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", agregarAlCarrito);
 });
