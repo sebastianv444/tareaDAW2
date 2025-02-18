5d09985278ec0b1ad0ee20e8c28db8326984c30c
sebastian <sebastian12verositi@gmail.com>
2025-02-18 13:45:15 +0100

    practicandoParaElExamenDeNodeDelMiercoles19

--- node/practicandoParaElExamen/crud_1/src/app.js
+++ node/practicandoParaElExamen/crud_1/src/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const config = require('./config/dotenv');
 const morgan = require('morgan');
 
-const alumnos = require('');
-
 const app = express();
+
+const alumnos = require('./routes/alumnos');
+
+// configuracion
+app.set('port', config.app.port);
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+// rutas
+app.use(alumnos);
--- node/practicandoParaElExamen/crud_1/src/index.js
+++ node/practicandoParaElExamen/crud_1/src/index.js
@@ -1,1 +1,5 @@
-console.log("buen npm!!!");
+const app = require('./app')
+
+app.listen(app.get('port'),()=>{
+    console.log("escuchando en el puerto: " + app.get('port'));
+});
